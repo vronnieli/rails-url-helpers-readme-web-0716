b0c3fc64a81bbbd4659c3300e1c97e991c09870c
vronnieli <vronnieli@gmail.com>
2016-08-20 13:50:04 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,9 @@
 class PostsController < ApplicationController
+
+	def new
+		@post = Post.new
+	end
+
 	def index
 		@posts = Post.all
 	end
@@ -6,4 +11,6 @@ class PostsController < ApplicationController
 	def show
 		@post = Post.find(params[:id])
 	end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index, :show]
+
+  get '/register', to: 'posts#new', as: 'register'
+    # get 'comments', as: 'comments'
 end
